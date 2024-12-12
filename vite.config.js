6360254c75b5f585b202dd677bfb3adc9d1a961d
Karim Ali <mikestudio2023@outlook.com>
2024-12-12 03:14:05 -0500

    Implemented CartItem, updated removeItem and updateQuantity

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/e-plantShopping",
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // Alias for src folder
-    },
-    extensions: ['.js', '.jsx'], // Ensure Vite resolves .jsx files as well
-  },
-});
+})
