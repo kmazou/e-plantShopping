bab3033fd7a7b2772fc0cd4e279f8d25b30c2ca1
Karim Ali <mikestudio2023@outlook.com>
2024-12-12 02:07:35 -0500

    Redux is correctly integrated

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-// src/main.js
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // Your global styles
-import { Provider } from 'react-redux';
-import store from './store.js'; // Redux store
+import App from './App.jsx'; // Your main app component
+import './index.css'; // Your CSS file
+import { Provider } from 'react-redux'; // Import Provider to connect Redux to your app
+import store from './store.js'; // Import the Redux store
 
+// Wrap the App component with the Provider to connect the Redux store
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store}> {/* Provide the Redux store to the entire app */}
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
-// src/redux/store.js
+// src/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartSlice';
+import cartReducer from './CartSlice'; // Import the cartReducer from CartSlice.jsx
 
+// Configure the Redux store
 const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: cartReducer, // The cart slice will be managed by cartReducer
   },
 });
 
-export default store;
+export default store; // Export the store so it can be used in the application
